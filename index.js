b78adf8ad487797302fe21866609d8be440963ca
unknown-tactics <unknown.tactics@atomicmail.io>
2026-05-11 01:46:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-const TARGET_URL = '91.99.223.225';
+const TARGET_URL = 'ph1t.future-aura.sbs';
 
 const keepAliveAgent = new http.Agent({
   keepAlive: true,
