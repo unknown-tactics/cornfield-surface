6ff6a749035e2f09dbc1e05ec0c1af97bf283b80
unknown-tactics <unknown.tactics@atomicmail.io>
2026-05-11 00:35:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const TARGET_URL = '91.99.223.225';
+
+const keepAliveAgent = new http.Agent({
+  keepAlive: true,
+  maxSockets: 100,
+  keepAliveMsecs: 3000,
+});
+
+const proxy = httpProxy.createProxyServer({
+  target: TARGET_URL,
+  changeOrigin: true,
+  secure: false,
+  xfwd: true,
+  agent: keepAliveAgent,
+  proxyTimeout: 0,
+  timeout: 0,
+});
+
+proxy.on('error', function (err, req, res) {
+  if (res && res.writeHead) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Relay Error.');
+  }
+});
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Azure TURBO Relay is Alive!');
+    return;
+  }
+  proxy.web(req, res);
+});
+
+server.on('upgrade', (req, socket, head) => {
+  proxy.ws(req, socket, head);
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Turbo Relay running on port ${PORT}`);
+});
